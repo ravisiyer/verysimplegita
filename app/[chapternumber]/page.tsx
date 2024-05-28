da7bfb5c2fde32b2275e06e69b228a9fbf90d696
ravisiyer <145967375+ravisiyer@users.noreply.github.com>
2024-05-28 20:46:05 +0530

    chapter verses shown

--- app/[chapternumber]/page.tsx
+++ app/[chapternumber]/page.tsx
@@ -1,6 +1,7 @@
 import { notFound } from "next/navigation";
 import { Suspense } from "react";
 import { getChapter } from "../lib/data";
+import Link from "next/link";
 
 async function Page({ params }) {
   const chapterNumber = params.chapternumber;
@@ -16,12 +17,26 @@ async function Page({ params }) {
   return (
     <div>
       <Suspense fallback={`Loading ...`}>
-        <h3>{`Chapter: ${chapterNumber}: ${gitaChapter.nameTranslated} ${gitaChapter.name}`}</h3>
+        <h3>{`Chapter ${chapterNumber}: ${gitaChapter.nameTranslated} ${gitaChapter.name}`}</h3>
         <h4>English Summary</h4>
         <p>{gitaChapter.chapterSummary}</p>
         <h4>हिन्दी सारांश</h4>
         <p>{gitaChapter.chapterSummaryHindi}</p>
         <h4>{`${gitaChapter.versesCount} verses`}</h4>
+        {gitaChapter.gitaVersesByChapterId.nodes.map((verse) => (
+          <div key={verse.id}>
+            {/* <Link href={`/${verse.verseNumber}`}> */}
+            <h3>{`Verse ${verse.verseNumber}`}</h3>
+            {/* </Link> */}
+            <h4>Text</h4>
+            <p>{verse.text}</p>
+            <h4>Transliteration</h4>
+            <p>{verse.transliteration}</p>
+            <h4>Word Meanings</h4>
+            <p>{verse.wordMeanings}</p>
+            <hr />
+          </div>
+        ))}
       </Suspense>
     </div>
   );
